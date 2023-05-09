8dc073d649f250e178d23e288d497741a5435c55
guion5 <127799018+guion5@users.noreply.github.com>
2023-05-08 22:27:37 -0300

    Update Media_Salas.cpp
    testando

--- Media_Salas.cpp
+++ Media_Salas.cpp
@@ -7,36 +7,35 @@ using namespace std;
 int main() {
     // Write C++ code here
     int sala1, sala2, sala3, sala4, sala5, sala6;
-    sala1 = 35;
-    sala2 = 4;
-    sala3 = 22;
-    sala4= 20;
-    sala5 = 36;
-    sala6 = 30;
+
+    cout << "Insira o nº de alunos da Sala 1: " << endl;
+    cin >> sala1;
     
+    cout << "Insira o nº de alunos da Sala 2: " << endl;
+    cin >> sala2;
+    
+    cout << "Insira o nº de alunos da Sala 3: " << endl;
+    cin >> sala3;
+    
+    cout << "Insira o nº de alunos da Sala 4: " << endl;
+    cin >> sala4;
+    
+    cout << "Insira o nº de alunos da Sala 5: " << endl;
+    cin >> sala5;
+    
+    cout << "Insira o nº de alunos da Sala 6: " << endl;
+    cin >> sala6;
+
     float media = (sala1 + sala2 + sala3 + sala4 + sala5 + sala6) / 6;
     cout << "A média de alunos é igual a " << media << endl;
+
+    if (sala1 > sala2 & sala1 > sala3 & sala1 > sala4 & sala1 > sala5 & sala1 > sala6);
+    cout << "A sala 1 possui o maior número de alunos" << endl;
     
-    if (sala1 >= media){
-        cout << "A sala 1 possui mais alunos que a média das salas" << endl;
-    }
-    if (sala2 >= media){
-        cout << "A sala 2 possui mais alunos que a média das salas" << endl;
-    }
-    if (sala3 >= media){
-        cout << "A sala 3 possui mais alunos que a média das salas"<< endl;
-    }  
-    if (sala4 >= media){
-        cout << "A sala 4 possui mais alunos que a média das salas" << endl; 
-    }    
-    if (sala5 >= media){
-        cout << "A sala 5 possui mais alunos que a média das salas" << endl;
-    }    
-    if (sala6 >= media){
-        cout << "A sala 6 possui mais alunos que a média das salas" << endl;        
-    }
+    if (sala2 > sala1 & sala2 > sala3 & sala2 > sala4 & sala2 > sala5 & sala2 > sala6);
+    cout << "A sala 2 possui o maior número de alunos" << endl;
     
     
-    
+
     return 0;
 }
