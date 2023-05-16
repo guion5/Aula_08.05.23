9984fbd2f1161fde092a4f2f20c96d30ae0f4b52
guion5 <127799018+guion5@users.noreply.github.com>
2023-05-15 21:47:06 -0300

    Update Media_Salas.cpp

--- Media_Salas.cpp
+++ Media_Salas.cpp
@@ -1,11 +1,12 @@
-// Online C++ compiler to run C++ program online
+// incluir bibliotecas
 #include <iostream>
 #include <stdio.h>
 #include <string>
 using namespace std;
 
+ // abertura do codigo 
 int main() {
-    // Write C++ code here
+   // variáveis
     int sala1, sala2, sala3, sala4, sala5, sala6;
     sala1 = 35;
     sala2 = 4;
@@ -14,10 +15,13 @@ int main() {
     sala5 = 36;
     sala6 = 30;
     
+    //calculo da media do numero de alunos
     float media = (sala1 + sala2 + sala3 + sala4 + sala5 + sala6) / 6;
     cout << "A média de alunos é igual a " << media << endl;
     
+    //condicionais para determinar a sala q possui mais alunos que a média
     if (sala1 >= media){
+        //retorno na interface do usuário
         cout << "A sala 1 possui mais alunos que a média das salas" << endl;
     }
     if (sala2 >= media){
@@ -35,8 +39,6 @@ int main() {
     if (sala6 >= media){
         cout << "A sala 6 possui mais alunos que a média das salas" << endl;        
     }
-    
-    
-    
+          
     return 0;
 }
